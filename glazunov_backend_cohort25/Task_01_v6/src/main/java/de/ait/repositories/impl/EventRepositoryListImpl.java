3753a6d6ea1e222a6c0a445fb025c4ce75140a72
Konstantin Glazunov <kglaz@ya.ru>
2023-08-26 23:41:06 +0200

    initial commit

---
+++ glazunov_backend_cohort25/Task_01_v6/src/main/java/de/ait/repositories/impl/EventRepositoryListImpl.java
@@ -0,0 +1,50 @@
+package de.ait.impl;
+
+import models.Event;
+import repositories.EventsRepository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventRepositoryListImpl implements EventsRepository {
+    private final List<Event> events = new ArrayList<>();
+    private Long generatedId = 1L;
+
+    @Override
+    public Event findBiId(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Event> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Event event) {
+        events.add(event);
+        event.setId(generatedId);
+        generatedId++;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public void upDate(Event model) {
+
+    }
+
+    @Override
+    public Event findByTitle(String titel) {
+        return null;
+    }
+
+    @Override
+    public Event findByStartDate(LocalDate startDate) {
+        return null;
+    }
+}
---
+++ glazunov_backend_cohort25/Task_01_v6/src/main/java/de/ait/services/impl/EventServiceImpl.java
@@ -0,0 +1,31 @@
+package de.ait.services.impl;
+
+import models.Event;
+import repositories.EventsRepository;
+import services.EventService;
+
+import java.time.LocalDate;
+
+public class EventServiceImpl implements EventService {
+
+    private final EventsRepository eventRepository;
+
+    public EventServiceImpl(EventsRepository userRepository) {
+        this.eventRepository = userRepository;
+    }
+
+
+    public Event createEvent(String title, LocalDate startDate, LocalDate endDate) {
+        if (title == null || title.equals("") || title.equals(" ")) {
+            throw new IllegalArgumentException("Titel is required field");
+        }
+        if (endDate.isBefore(startDate)) {
+            throw new IllegalArgumentException("End data can not be before start data");
+        }
+        Event event = new Event(title, startDate, endDate);
+        eventRepository.save(event);
+        return event;
+
+    }
+
+}
